625f55795523d2cf0b4e3ffac21c8be40cdc6a7e
igorbavand <igor.bvn@gmail.com>
2019-12-11 23:33:11 -0300

    corrigir form cadastro

---
+++ cadastro.php
@@ -0,0 +1,130 @@
+<?php
+session_start();
+include('conexao/conexao.php');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SCME - Sistema UFC</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		-->
+		
+
+		
+	</head>
+	<body>
+
+		<div class="container">
+			<div class="text-center border" style="margin-top: 2%">
+			<h1 style="color: white">Bem-Vindo ao SCME</h1>
+			<p style="color: white; font-size: 15px; ">Sistema de Controle de Monitoria e Estudos</p>
+		</div>
+		<?php
+			if (isset($_SESSION['nao_autenticado'])) {
+		?>
+		<div class="alert alert-warning">
+			Erro: usuário e/ou senha inválido(s)
+		</div>
+	<?php
+	unset($_SESSION['nao_autenticado']);
+	 } ?>
+	
+	<div id="conteudo" class="d-flex justify-content-center h-100" style="margin-top: 3px">
+	
+		
+			
+
+
+
+<div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Cadastro</h5>
+           
+           <form method="post" action="cadastrar.php">
+<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" name="nome" class="form-control" placeholder="nome completo">
+						
+					</div>
+
+
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" name="usuario" class="form-control" placeholder="usuário">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" name="senha" class="form-control" placeholder="senha">
+					</div>
+
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" name="csenha" class="form-control" placeholder="confirmar senha">
+					</div>
+
+
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-book-reader"></i></span>
+						</div>
+						<input type="text" name="curso" class="form-control" placeholder="curso">
+						
+					</div>
+
+
+				<div class="input-group">
+				  <div class="input-group-prepend">
+				    <span class="input-group-text fas fa-camera" id="inputGroupFileAddon01"></span>
+				  </div>
+				  <div class="custom-file">
+				    <input type="file" name="foto" class="custom-file-input" id="inputGroupFile01"
+				      aria-describedby="inputGroupFileAddon01">
+				    <label class="custom-file-label" for="inputGroupFile01">Selecione uma imagem</label>
+				  </div>
+				</div>
+					
+					<div class="form-group">
+						<input type="submit" value="Cadastrar" class="btn btn-lg btn-primary btn-block text-uppercase">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				
+				<div id="menu" class="d-flex justify-content-center links">
+					Já tem conta?  <a href="index.php">ENTRE</a>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+
+          </div>
+        
+      
+    </div>
+  </div>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -17,21 +17,17 @@ include('conexao/conexao.php');
 	<link rel="stylesheet" type="text/css" href="styles.css">
 	<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
 		-->
+
+		<script
+			  src="https://code.jquery.com/jquery-1.10.2.min.js"
+			  integrity="sha256-C6CB9UYIS9UJeqinPHWTHVqh/E1uhG5Twh+Y5qFQmYg="
+			  crossorigin="anonymous"></script>
+
+			  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js">
 		<style type="text/css">
-			body{
-				background-color: blue;
-			}
+			
 
-@import url('https://fonts.googleapis.com/css?family=Numans');
-
-html,body{
-background-image: url('http://getwallpapers.com/wallpaper/full/d/e/c/368192.jpg');
-background-size: cover;
-background-repeat: no-repeat;
-height: 100vh;
-font-family: 'Numans', sans-serif;
-}
-
+/*
 .container{
 height: 100%;
 align-content: center;
@@ -112,8 +108,152 @@ margin-left: 4px;
 	color: white;
 }div a:hover{
 	color: white;
+}*/
+:root {
+  --input-padding-x: 1.5rem;
+  --input-padding-y: .75rem;
+}
+
+body {
+  background: #007bff;
+  background: linear-gradient(to right, #0062E6, #33AEFF);
+}
+
+.card-signin {
+  border: 0;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.card-signin .card-title {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  height: auto;
+  border-radius: 2rem;
+}
+
+.form-label-group>input,
+.form-label-group>label {
+  padding: var(--input-padding-y) var(--input-padding-x);
+}
+
+.form-label-group>label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0;
+  /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  transition: all .1s ease-in-out;
+}
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+  padding-bottom: calc(var(--input-padding-y) / 3);
+}
+
+.form-label-group input:not(:placeholder-shown)~label {
+  padding-top: calc(var(--input-padding-y) / 3);
+  padding-bottom: calc(var(--input-padding-y) / 3);
+  font-size: 12px;
+  color: #777;
+}
+
+.btn-google {
+  color: white;
+  background-color: #ea4335;
+}
+
+.btn-facebook {
+  color: white;
+  background-color: #3b5998;
+}
+
+/* Fallback for Edge
+-------------------------------------------------- */
+
+@supports (-ms-ime-align: auto) {
+  .form-label-group>label {
+    display: none;
+  }
+  .form-label-group input::-ms-input-placeholder {
+    color: #777;
+  }
+}
+
+/* Fallback for IE
+-------------------------------------------------- */
+
+@media all and (-ms-high-contrast: none),
+(-ms-high-contrast: active) {
+  .form-label-group>label {
+    display: none;
+  }
+  .form-label-group input:-ms-input-placeholder {
+    color: #777;
+  }
 }
 		</style>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$("#menu a").click(function( e ){
+					e.preventDefault();
+					var href = $( this ).attr('href');
+					$("#conteudo").load( href +" #conteudo")
+				});
+			});
+		</script>
 	</head>
 	<body>
 
@@ -132,7 +272,7 @@ margin-left: 4px;
 	unset($_SESSION['nao_autenticado']);
 	 } ?>
 	
-	<div class="d-flex justify-content-center h-100" style="margin-top: 3px">
+	<div id="conteudo" class="d-flex justify-content-center h-100" style="margin-top: 3px">
 		
 		<div class="card">
 			
@@ -156,7 +296,7 @@ margin-left: 4px;
 					</div>
 					
 					<div class="form-group">
-						<input type="submit" value="Entrar" class="btn float-right login_btn">
+						<input type="submit" value="Entrar" class="btn btn-lg btn-primary btn-block text-uppercase">
 					</div>
 				</form>
 			</div>
@@ -164,8 +304,8 @@ margin-left: 4px;
 				<div class="d-flex justify-content-center">
 					<a href="#">Esqueceu a senha?</a>
 				</div>
-				<div class="d-flex justify-content-center links">
-					Não tem conta? <a href="#">CADASTRE-SE</a>
+				<div id="menu" class="d-flex justify-content-center links">
+					Não tem conta?  <a href="cadastro.php">CADASTRE-SE</a>
 				</div>
 				
 			</div>
